bbb8c60883fe775eae8e9db32a9db6b883e68e85
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-20 19:23:41 +0200

    🖌 Font and background

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h2>Hello world</h2>
+    <h1>Hello world</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
