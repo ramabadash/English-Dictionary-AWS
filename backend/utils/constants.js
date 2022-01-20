12e03f196fce630332aff101fdac305bcd888022
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-20 15:28:22 +0200

    📍 GET/:word/:partOfSpeech - get the word with the part of speech from the url params

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,3 +1,14 @@
 const TABLE_NAME = 'dictionary';
 
-module.exports = { TABLE_NAME };
+const PART_OF_SPEECH_DICT = {
+  adjectives: 'a.',
+  adverbs: 'adv.',
+  interjections: 'interj.',
+  nouns: 'n.',
+  verbs: 'v.',
+  pronouns: 'pron.',
+  prepositions: 'prep.',
+  conjunctions: 'conj.',
+};
+
+module.exports = { TABLE_NAME, PART_OF_SPEECH_DICT };
