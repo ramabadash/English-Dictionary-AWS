614b38e7c11a75466a88bd899f5ac607d6d31ac1
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-20 14:35:37 +0200

    📂 Move index.js to backend dir

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+// Routers
+const { wordsRouter } = require('./routers/wordsRouter');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use('/', wordsRouter);
 
 module.exports.handler = serverless(app);
