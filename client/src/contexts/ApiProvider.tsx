97c33691752b3c8bda2a5f074a8235d41f0f17d0
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-22 19:52:56 +0200

    🔸 Add getRandWordByPart function to do api request for /part-of-speech endpoints

--- client/src/contexts/ApiProvider.tsx
+++ client/src/contexts/ApiProvider.tsx
@@ -10,7 +10,8 @@ function ApiProvider({ children }: { children: JSX.Element[] | JSX.Element }) {
   const [words, setWords] = useState<WordObj[]>([]); // Arr of word result
   const [loading, setLoading] = useState(false); // Is loading
 
-  /***** FUNCTIONS *****/
+  /***** FUNCTIONS - NETWORKING *****/
+
   // Get word from dictionary by the word
   const getWord = async (word: string, partOfSpeech: string | undefined) => {
     setLoading(true); // Start loader
@@ -31,8 +32,31 @@ function ApiProvider({ children }: { children: JSX.Element[] | JSX.Element }) {
     }
   };
 
+  // Get random word by part of speech and (optional) by letter
+  const getRandWordByPart = async (
+    partOfSpeech: string,
+    letter: string | undefined
+  ) => {
+    setLoading(true); // Start loader
+    try {
+      const requestUrl = letter
+        ? `http://localhost:3000/part-of-speech/${partOfSpeech}/${letter}`
+        : `http://localhost:3000/part-of-speech/${partOfSpeech}`;
+
+      const { data } = await axios.get(requestUrl);
+      setWords(data); // Set result
+
+      // Stop loader
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    } catch (error) {
+      console.log(error); // TODO ERROR HANDLING
+    }
+  };
+
   return (
-    <ApiContext.Provider value={{ getWord, words, loading }}>
+    <ApiContext.Provider value={{ getWord, getRandWordByPart, words, loading }}>
       {children}
     </ApiContext.Provider>
   );
