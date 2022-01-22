e94a3cf7d7bb60f3bdf119f307686bfdb3c7e3a6
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-22 19:54:45 +0200

    🔸 Add getRandWordByPart function top the AppContextInterface interface

--- client/src/@types/types.ts
+++ client/src/@types/types.ts
@@ -6,6 +6,10 @@ export interface WordObj {
 
 export interface AppContextInterface {
   getWord?: (word: string, partOfSpeech: string | undefined) => void;
+  getRandWordByPart?: (
+    partOfSpeech: string,
+    letter: string | undefined
+  ) => void;
   words?: WordObj[];
   loading?: boolean;
 }
